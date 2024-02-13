854a9f4e2ed299a586439237f3f18d200c848fb4
PrasannaP <mail2lulu@gmail.com>
2024-02-13 17:15:45 +0530

    animejs2

--- animejs/basic-animationanime-js/script.js
+++ animejs/basic-animationanime-js/script.js
@@ -1,8 +1,24 @@
-var myAnimation = anime({
+var myMolProtien = anime({
   targets: ['.blue', '.green'],
   translateX: '13rem',
   rotate: 180,
   borderRadius: '8px',
-  duration: 2000,
+  duration: 3000,
   loop: true
 });
+
+var myMolProtien2 = anime({
+  targets: ['.blue', '.green'],
+  keyframes: [
+    { translateY: -40 },
+    { translateX: 250 },
+    { translateY: 40 },
+    { translateX: 0 },
+    { translateY: 0 }
+  ],
+  duration: 2000,
+  // easing: 'easeOutElastic(1, .8)',
+  loop: true
+});
+myMolProtien2.pause();
+// myMolProtien.pause()
