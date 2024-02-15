fe72d0954cb465e6101fa8d74fd9c4d1ad8495e5
PrasannaP <mail2lulu@gmail.com>
2024-02-15 13:49:44 +0530

    createMolecule on click done

--- gasp1/simple1/readme.md
+++ gasp1/simple1/readme.md
@@ -1,2 +1,5 @@
 ## bug move
 https://codepen.io/GreenSock/pen/vYPdaOm?editors=1112
+
+## accessing-repeat-counter
+https://gsap.com/community/forums/topic/14115-accessing-repeat-counter/
--- gasp1/simple1/script.js
+++ gasp1/simple1/script.js
@@ -1,16 +1,23 @@
 
 
-gsap.registerPlugin(GSDevTools)
-GSDevTools.create({ paused: false, id: "Hero Animation" });
+// gsap.registerPlugin(GSDevTools)
+// GSDevTools.create({ paused: false, id: "Hero Animation" });
 
 let tls = []
-let pointsArr = [{ x: 390, y: 82 }, { x: 125, y: 85 }, { x: 105, y: 202 }, { x: 180, y: 300 }, { x: 29, y: 300 }, { x: 191, y: 304 }, { x: 189, y: 197 }, { x: 111, y: 199 }, { x: 111, y: 405 }, { x: 26, y: 409 }, { x: 22, y: 678 }, { x: 192, y: 677 }, { x: 195, y: 402 }, { x: 110, y: 400 }, { x: 110, y: 461 }, { x: 280, y: 458 }, { x: 298, y: 518 }, { x: 435, y: 529 }, { x: 433, y: 618 }, { x: 287, y: 622 }, { x: 286, y: 683 }, { x: 579, y: 682 }, { x: 578, y: 618 }, { x: 436, y: 617 }, { x: 437, y: 527 }, { x: 573, y: 520 }, { x: 589, y: 454 }, { x: 845, y: 455 }, { x: 843, y: 674 }, { x: 684, y: 678 }, { x: 678, y: 451 }, { x: 586, y: 451 }, { x: 590, y: 274 }, { x: 676, y: 270 }, { x: 677, y: 209 }, { x: 843, y: 208 }, { x: 848, y: 335 }, { x: 678, y: 337 }, { x: 673, y: 263 }, { x: 585, y: 270 }, { x: 575, y: 219 }, { x: 368, y: 208 }, { x: 364, y: 324 }, { x: 506, y: 327 }, { x: 509, y: 214 }, { x: 298, y: 228 }, { x: 283, y: 252 }, { x: 278, y: 453 }, { x: 237, y: 452 }]
+// let pointsArr = [{ x: 390, y: 82 }, { x: 125, y: 85 }, { x: 105, y: 202 }, { x: 180, y: 300 }, { x: 29, y: 300 }, { x: 191, y: 304 }, { x: 189, y: 197 }, { x: 111, y: 199 }, { x: 111, y: 405 }, { x: 26, y: 409 }, { x: 22, y: 678 }, { x: 192, y: 677 }, { x: 195, y: 402 }, { x: 110, y: 400 }, { x: 110, y: 461 }, { x: 280, y: 458 }, { x: 298, y: 518 }, { x: 435, y: 529 }, { x: 433, y: 618 }, { x: 287, y: 622 }, { x: 286, y: 683 }, { x: 579, y: 682 }, { x: 578, y: 618 }, { x: 436, y: 617 }, { x: 437, y: 527 }, { x: 573, y: 520 }, { x: 589, y: 454 }, { x: 845, y: 455 }, { x: 843, y: 674 }, { x: 684, y: 678 }, { x: 678, y: 451 }, { x: 586, y: 451 }, { x: 590, y: 274 }, { x: 676, y: 270 }, { x: 677, y: 209 }, { x: 843, y: 208 }, { x: 848, y: 335 }, { x: 678, y: 337 }, { x: 673, y: 263 }, { x: 585, y: 270 }, { x: 575, y: 219 }, { x: 368, y: 208 }, { x: 364, y: 324 }, { x: 506, y: 327 }, { x: 509, y: 214 }, { x: 298, y: 228 }, { x: 283, y: 252 }, { x: 278, y: 453 }, { x: 237, y: 452 }]
+
+
+btn.addEventListener("click", createMolecule);
 
 function createMolecule(molName, counts) {
 
-    setTimeout(() => {
+    var target = document.createElement("div");
+    target.classList.add("anim");
+    target.classList.add("protein");
+    document.body.appendChild(target);
 
-        let tw1 = gsap.to(".class1", {
+    setTimeout(() => {
+        let tw1 = gsap.to(target, {
             x: function (ctr) {
                 var repeatCount = Math.floor(tw1.totalTime() / (tw1.duration() + tw1.repeatDelay()));
                 return pointsArr[repeatCount].x
@@ -22,11 +29,7 @@ function createMolecule(molName, counts) {
             repeat: pointsArr.length - 1,
             repeatRefresh: true
         })
-    }, 200);
-
-
-    version.textContent = gsap.version
-
+    }, 500);
 }
 function createMolecule2(molName, counts) {
 
@@ -54,7 +57,7 @@ function createMolecule2(molName, counts) {
 // delete
 // group animation
 
-createMolecule("p", 4)
+// createMolecule("p", 4)
 // let arrayP = []
 // for (let index = 0; index < arrayP.length; index++) {
 //     const element = arrayP[index];
